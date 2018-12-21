27d9d0c422ec047611f0228b511c4f9be5cb0f0e
LR-LR <elhaire.aboudou@gmail.com>
2018-12-21 17:46:55 +0100

    probleme police ubuntu résolu

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-// ---------- INDEX.HTML ---------- \\
-// Variables
-let indexTitle = document.getElementById("index-title");
-let indexNav1 = document.getElementById("index-nav-1");
-let indexNav2 = document.getElementById("index-nav-2");
+document.addEventListener("DOMContentLoaded", function(event) {
+  // ---------- INDEX.HTML ---------- \\
+  // Variables
+  let indexTitle = document.getElementById("index-title");
+  let indexNav1 = document.getElementById("index-nav-1");
+  let indexNav2 = document.getElementById("index-nav-2");
 
-// First link hover
-indexNav1.addEventListener("mouseenter", function(){
-  indexTitle.style.opacity = "0.5";
-  indexNav1.style.opacity = "1";
-  indexNav2.style.opacity = "0.5";
-}, false);
-indexNav1.addEventListener("mouseleave", function(){
-  indexTitle.style.opacity = "1";
-  indexNav1.style.opacity = "1";
-  indexNav2.style.opacity = "1";
-}, false);
+  // First link hover
+  indexNav1.addEventListener("mouseenter", function(){
+    indexTitle.style.opacity = "0.2";
+    indexNav1.style.opacity = "1";
+    indexNav2.style.opacity = "0.2";
+  }, false);
+  indexNav1.addEventListener("mouseleave", function(){
+    indexTitle.style.opacity = "1";
+    indexNav1.style.opacity = "1";
+    indexNav2.style.opacity = "1";
+  }, false);
 
-// Second link hover
-indexNav2.addEventListener("mouseenter", function(){
-  indexTitle.style.opacity = "0.5";
-  indexNav1.style.opacity = "0.5";
-  indexNav2.style.opacity = "1";
-}, false);
-indexNav2.addEventListener("mouseleave", function(){
-  indexTitle.style.opacity = "1";
-  indexNav1.style.opacity = "1";
-  indexNav2.style.opacity = "1";
-}, false);
+  // Second link hover
+  indexNav2.addEventListener("mouseenter", function(){
+    indexTitle.style.opacity = "0.2";
+    indexNav1.style.opacity = "0.2";
+    indexNav2.style.opacity = "1";
+  }, false);
+  indexNav2.addEventListener("mouseleave", function(){
+    indexTitle.style.opacity = "1";
+    indexNav1.style.opacity = "1";
+    indexNav2.style.opacity = "1";
+  }, false);
+});
