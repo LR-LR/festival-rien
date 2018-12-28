f2703518bd69414da5754a3069d8fbeb99467a7d
LR-LR <elhaire.aboudou@gmail.com>
2018-12-28 14:36:02 +0100

    page programmation presque terminée

--- js/script-index.js
+++ js/script-index.js
@@ -5,27 +5,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let indexNav1 = document.getElementById("index-nav-1");
   let indexNav2 = document.getElementById("index-nav-2");
 
-  // First link hover
-  indexNav1.addEventListener("mouseenter", function(){
+  // Events
+  // indexNav1 hover
+  indexNav1.addEventListener("mouseenter", function() {
     indexTitle.style.opacity = "0.2";
     indexNav1.style.opacity = "1";
     indexNav2.style.opacity = "0.2";
   }, false);
-  indexNav1.addEventListener("mouseleave", function(){
-    indexTitle.style.opacity = "1";
-    indexNav1.style.opacity = "1";
-    indexNav2.style.opacity = "1";
-  }, false);
+  indexNav1.addEventListener("mouseleave", resetOpacity, false);
 
-  // Second link hover
-  indexNav2.addEventListener("mouseenter", function(){
+  // indexNav2 hover
+  indexNav2.addEventListener("mouseenter", function() {
     indexTitle.style.opacity = "0.2";
     indexNav1.style.opacity = "0.2";
     indexNav2.style.opacity = "1";
   }, false);
-  indexNav2.addEventListener("mouseleave", function(){
+  indexNav2.addEventListener("mouseleave", resetOpacity, false);
+
+  // function resetOpacity
+  function resetOpacity(){
     indexTitle.style.opacity = "1";
     indexNav1.style.opacity = "1";
     indexNav2.style.opacity = "1";
-  }, false);
+  }
 });
---
+++ js/script-prog-nav.js
@@ -0,0 +1,107 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  // ---------- PROGRAMMATION-NAV.HTML ---------- \\
+  // Variables
+  let navDay1 = document.getElementById("nav-day-1");
+  let navDay2 = document.getElementById("nav-day-2");
+  let navDay3 = document.getElementById("nav-day-3");
+  let navDay4 = document.getElementById("nav-day-4");
+  let navDay5 = document.getElementById("nav-day-5");
+  let homeButton = document.getElementById("home-button");
+  let backButton = document.getElementById("back-button");
+
+  // Events
+  // navDay1 hover
+  navDay1.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "1";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "0.2";
+  }, false);
+  navDay1.addEventListener("mouseleave", resetOpacity, false);
+
+  // navDay2 hover
+  navDay2.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "1";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "0.2";
+  }, false);
+  navDay2.addEventListener("mouseleave", resetOpacity, false);
+
+  // navDay3 hover
+  navDay3.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "1";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "0.2";
+  }, false);
+  navDay3.addEventListener("mouseleave", resetOpacity, false);
+
+  // navDay4 hover
+  navDay4.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "1";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "0.2";
+  }, false);
+  navDay4.addEventListener("mouseleave", resetOpacity, false);
+
+  // navDay5 hover
+  navDay5.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "1";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "0.2";
+  }, false);
+  navDay5.addEventListener("mouseleave", resetOpacity, false);
+
+  // homeButton hover
+  homeButton.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "1";
+    backButton.style.opacity = "0.2";
+  }, false);
+  homeButton.addEventListener("mouseleave", resetOpacity, false);
+
+  // backButton hover
+  backButton.addEventListener("mouseenter", function(){
+    navDay1.style.opacity = "0.2";
+    navDay2.style.opacity = "0.2";
+    navDay3.style.opacity = "0.2";
+    navDay4.style.opacity = "0.2";
+    navDay5.style.opacity = "0.2";
+    homeButton.style.opacity = "0.2";
+    backButton.style.opacity = "1";
+  }, false);
+  backButton.addEventListener("mouseleave", resetOpacity, false);
+
+  // function resetOpacity
+  function resetOpacity(){
+    navDay1.style.opacity = "inherit";
+    navDay2.style.opacity = "inherit";
+    navDay3.style.opacity = "inherit";
+    navDay4.style.opacity = "inherit";
+    navDay5.style.opacity = "inherit";
+    homeButton.style.opacity = "inherit";
+    backButton.style.opacity = "inherit";
+  };
+});
